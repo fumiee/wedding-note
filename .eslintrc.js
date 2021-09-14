d6afc32dd0b08e79359c846d3f53690e1bb11e3d
fumiee <fumie.yori@gmail.com>
2021-09-14 16:44:53 +0900

    🔨 setting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,62 @@
 module.exports = {
-  env: { es2021: true, browser: true, jest: true, node: true },
-  plugins: ["import", "simple-import-sort", "react-hooks"], //importをアルファベット順に並べ替える
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./tsconfig.json" },
+  settings: { tailwindcss: { groupByResponsive: true } },
+  plugins: ["simple-import-sort", "tailwindcss", "import-access"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:jsx-a11y/recommended",
-    "next",
+    "plugin:tailwindcss/recommended",
     "next/core-web-vitals",
     "prettier",
-    "plugin:import/recommended",
-  ], //外部パッケージの指定をするとparserOptions,plugins,envなど他部分も描き変わる
-
-  parser: "@typescript-eslint/parser",
-  parserOptions: { project: "./tsconfig.json", sourceType: "module" }, //構文のルールの追加
+  ],
   rules: {
-    "no-console": ["error", { allow: ["warn", "info", "error"] }], //consoleは書かない
-    "prefer-arrow-callback": "error", //アロー関数を使う
-    "prefer-const": "error", //letよりconstを使う
-    "arrow-body-style": ["off", "always"], //returnをつけよう
-    "react/prop-types": "off", //typescriptの場合は不要
+    "no-console": ["error", { allow: ["warn", "info", "error"] }],
+    "no-restricted-syntax": ["error", { selector: "TSEnumDeclaration", message: "Don't declare enums" }],
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "func-style": ["error", "expression"],
+    "arrow-body-style": ["error", "always"],
+    "no-restricted-imports": ["error", { paths: [{ name: "react", importNames: ["default"] }] }],
+    "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
-    "react/display-name": "off",
+    "react/display-name": "error",
+    "react/jsx-handler-names": [
+      "error",
+      {
+        eventHandlerPrefix: "handle",
+        eventHandlerPropPrefix: "on",
+        checkLocalVariables: true,
+        checkInlineFunction: true,
+      },
+    ],
     "react/destructuring-assignment": ["error", "never"],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "off", //hooksの第二引数が空などを検出
-    "import/newline-after-import": "error", //importの後は１行空ける
-    "import/no-default-export": "error", //defaultexportの使用
-    "simple-import-sort/imports": "warn", //import文を並べ替える
-    "simple-import-sort/exports": "warn", //export文を並べ替える
-    "@typescript-eslint/no-explicit-any": "warn", //anyを明示的に使わない
-    "@typescript-eslint/no-var-requires": "off", //importステートメントを除いてrequire禁止
-    "@typescript-eslint/explicit-module-boundary-types": "off", //exportする関数の型の明記が必須
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports" },
+    "react-hooks/exhaustive-deps": "warn",
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "import-access/jsdoc": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports" }],
+    "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: ["typeAlias", "typeParameter"], format: ["PascalCase"] },
+      { selector: ["method"], format: ["camelCase"] },
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase"],
+        prefix: ["no", "is", "has", "should"],
+        filter: { regex: "^_", match: false },
+      },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error", //使っていない関数の検出
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-    ],
-    "jsx-a11y/no-autofocus": "off", //autofocusの使用
+    "jsx-a11y/no-autofocus": "off",
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
@@ -48,8 +65,7 @@ module.exports = {
         aspects: ["invalidHref", "preferButton"],
       },
     ],
-    "import/no-cycle": "error",
-  }, //アンダーバーをリンクじゃない所で使用しない
+  },
   overrides: [
     {
       files: ["src/pages/**/*.tsx", "src/pages/api/**/*.ts"],
