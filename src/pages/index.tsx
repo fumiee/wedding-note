0b9302b0ee231afa3678729a63fb0a4f333e907b
fumiee <fumie.yori@gmail.com>
2021-09-17 00:16:05 +0900

    🏗️ chenge index-page to loginpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,31 @@
-import { Contents } from "src/components/Contents";
-import { Entry } from "src/components/Entry";
-import { Footer } from "src/components/Footer";
+import { useState, useEffect } from "react";
+import { supabase } from "src/libs/supabase";
+import { Auth } from "src/components/Auth";
+import { Account } from "src/components/Account";
 import { Header } from "src/components/Header";
-import { Top } from "src/components/Top";
+import { Footer } from "src/components/Footer";
+import type { Session } from "@supabase/supabase-js";
 
-const Home = () => {
+const Home: React.VFC = () => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    // setSession(supabase.auth.session());
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <Top />
-      <Contents />
-      <Entry />
-      <Footer />
+    <div className="flex flex-col min-h-screen">
+      <div>
+        <Header />
+      </div>
+      <div className="flex-1">{!session ? <Auth /> : <Account />}</div>
+      <div className="h-48">
+        <Footer />
+      </div>
     </div>
   );
 };
-
 export default Home;
