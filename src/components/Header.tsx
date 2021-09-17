1898ef5f76330ef144bf78870c5523f2fac55265
fumiee <fumie.yori@gmail.com>
2021-09-17 09:44:07 +0900

    ✨ connect links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,20 @@
-import { IoIosSearch } from "react-icons/io";
-import { AiOutlineUser } from "react-icons/ai";
 import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineUser } from "react-icons/ai";
+import { IoIosSearch } from "react-icons/io";
+
 import logo from "/public/logo.svg";
 
 export const Header: React.VFC = () => {
   return (
     <div className="flex justify-between p-4">
       <IoIosSearch size={30} color={"#5A5A5A"} className="mt-1" />
-      <Image src={logo} alt="logo" height={40} width={203} />
-      <AiOutlineUser size={30} color={"#5A5A5A"} className="mt-1" />
+      <Link href="/top" passHref>
+        <Image src={logo} alt="logo" height={40} width={203} />
+      </Link>
+      <Link href="/" passHref>
+        <AiOutlineUser size={30} color={"#5A5A5A"} className="mt-1" />
+      </Link>
     </div>
   );
 };
