6c9a557ca604375661ae206114e3ca295c479b87
fumiee <fumie.yori@gmail.com>
2021-09-20 15:21:26 +0900

    ✨ set LoginedHeader and Footer

--- src/pages/myPage.tsx
+++ src/pages/myPage.tsx
@@ -1,9 +1,15 @@
+import { Footer } from "src/components/Footer";
+import { LoginedHeader } from "src/components/LoginedHeader";
 import { User } from "src/components/User";
 
 const MyPage: React.VFC = () => {
   return (
-    <div>
-      <User />
+    <div className="flex flex-col min-h-screen">
+      <LoginedHeader />
+      <div className="flex-1">
+        <User />
+      </div>
+      <Footer />
     </div>
   );
 };
