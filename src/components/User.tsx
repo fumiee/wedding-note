e2eb9c2458fdb72b1863924efdeeaed29a9f2b56
fumiee <fumie.yori@gmail.com>
2021-09-20 15:23:00 +0900

    💄 add item

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,75 +4,92 @@ import type { definitions } from "src/types/supabase";
 import Image from "next/image";
 
 type Profile = Pick<definitions["profiles"], "name" | "avatar">;
+type Update = Pick<definitions["profiles"], "name" | "avatar" | "wedding_hall" | "description" | "updated_at">;
 
 export const User: React.VFC<Profile> = () => {
   const [profile, setProfile] = useState<Profile>();
 
-  // useEffect(() => {
-  //   // setSession(supabase.auth.session());
-  //   supabase.auth.onAuthStateChange((_event, session) => {
-  //     session;
-  //   });
-  // }, []);
-
   useEffect(() => {
     fetchProfiles();
+    console.log(1);
   }, []);
 
   const fetchProfiles = async () => {
     const user = supabase.auth.user();
     if (!user) return;
     try {
-      const { data, error } = await supabase
+      const res = await supabase
         .from<definitions["profiles"]>("profiles")
         .select("name,avatar")
         .eq("user_id", user.id)
         .single();
-      if (!data) throw new Error("ユーザーデータがありません"); //オプショナルインスタンス クラスのインスタンス化
-      setProfile(data); //userがあるならdataは絶対あるのに？！は、そうしかない状態を作る。
-      // console.log(error);
-      if (error) throw error; //下のerrorはとってきたerror？ちがう。これ
+      if (res.error) throw res.error;
+      setProfile(res.data);
     } catch (error) {
-      console.log("error", error);
+      console.error("error", error);
     }
   };
 
-  // const updateProfile=({name,avatar,wedding_holl,description})=>{
-  //     try{
-  //         const user = supabase.auth.user()
-  //         const updates = {
-  //         name,
-  //         avatar,
-  //         wedding_holl,
-  //         description,
-  //         update_at:new Date(),
-  //         }
-  //     }
-  // }
+  const signOut = () => {
+    supabase.auth.signOut();
+    setProfile(undefined);
+  };
+
+  const updateProfile = ({ name, avatar, wedding_hall, description }: Update) => {
+    try {
+      // const user = supabase.auth.user();
+      const updates = {
+        name,
+        avatar,
+        wedding_hall,
+        description,
+        update_at: new Date(),
+      };
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
 
   return (
-    <div>
-      <div className="flex justify-center">
+    <div className="m-auto space-y-8 max-w-90">
+      <div className="flex justify-around">
         <div>
           {profile?.avatar ? (
-            <Image src={profile.avatar} alt="avatar" style={{ height: 100, width: 100 }} className="m-5 rounded-full" />
+            <Image src={profile.avatar} alt="avatar" height={100} width={100} className="rounded-full" />
           ) : (
             <div className="w-28 h-28 bg-gray-200 rounded-full" />
           )}
         </div>
         <div>
-          <button
-            className=" mb-6 w-36 text-gray-400 border-2"
-            onClick={() => {
-              return supabase.auth.signOut();
-            }}
-          >
-            Sign Out
-          </button>
+          <div className="flex justify-around mb-5">
+            <button className="px-2 text-gray-400 border-2" onClick={updateProfile}>
+              Up date
+            </button>
+            <button className=" px-2 text-gray-400 border-2" onClick={signOut}>
+              Sign Out
+            </button>
+          </div>
           <p></p>
-          <label htmlFor="name">name</label>
-          <input type="text" id="name" defaultValue={profile?.name} className="text-center border-b-2" />
+          <label htmlFor="name" className="flex justify-start mb-5">
+            name
+          </label>
+          <p />
+          <input type="text" id="name" defaultValue={profile?.name} className="w-full text-center border-b-2" />
         </div>
+      </div>
+      <div>
+        <label htmlFor="name" className="flex justify-start">
+          wedding hall
+        </label>
+        <p />
+        <input type="text" id="hall" className="m-auto w-full border-b-2" />
+      </div>
+      <div>
+        <label htmlFor="comment" className="flex justify-start">
+          comment
+        </label>
+        <p />
+        <input type="text" id="comment" className="w-full border-b-2" />
       </div>
     </div>
   );
