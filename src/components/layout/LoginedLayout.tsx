2e24d167bece1123f49e1e455aa1b9be39e6d704
fumiee <fumie.yori@gmail.com>
2021-10-06 22:00:43 +0900

    💄 add z-index

--- src/components/layout/LoginedLayout.tsx
+++ src/components/layout/LoginedLayout.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const LoginedLayout = (props: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="sticky top-0">
+      <div className="sticky top-0 z-50">
         <LoginedHeader />
       </div>
       <div className="flex-1">{props.children}</div>
