e73a809ede8c3a2807eb4ca977de66965f27d1b3
fumiee <fumie.yori@gmail.com>
2021-10-10 18:45:05 +0900

    💄 improvement UI,comment

--- src/components/intro/Contents.tsx
+++ src/components/intro/Contents.tsx
@@ -12,22 +12,18 @@ const ITEM = [
   {
     icon: <GiPencil size={50} color={"#5A5A5A"} className="m-auto" />,
     head: "キロク",
-    text: `各打ち合わせごとに記録したり、ドレスや装花などカテゴリー別にしたりと
-    自由に記録できます。その時その時の思いや考えを文字にすることで頭の中が整理できます。`,
+    text: `打ち合わせ内容を記録したり思いや考えを自由に。
+    後から振り返るのも楽しいものです。`,
   },
   {
     icon: <BsBook size={50} color={"#5A5A5A"} className="m-auto" />,
     head: "ヨム",
-    text: `他の花嫁さまの記録を閲覧できます。
-    同じ都道府県の花嫁さまの記録から好みのドレスショップを見つけたり
-    同じ式場の花嫁さまからのアドバイスが見れるかもしれません。`,
+    text: `他の花嫁さまのキロクからヒントを得たり、同じ境遇の方を見つけたり。`,
   },
   {
     icon: <BsCheck size={50} color={"#5A5A5A"} className="m-auto" />,
     head: "カクニン",
-    text: `次の打ち合わせでプランナーさんに確認することや買うべきものを一覧に。終わったことにはチェックを入れて。
-    他の花嫁さまの記録を読んでいると式場に確認しなきゃ！わたしもこれしたい！がきっとでてきます。
-    そんなときはすぐに追加しておきましょう。`,
+    text: `TODOリストでしっかり確認。漏れをなくしましょう。`,
   },
 ];
 
@@ -38,10 +34,10 @@ export const Contents: React.VFC = () => {
       <p className="mb-20">{"wedding noteでできること"}</p>
       {ITEM.map((item) => {
         return (
-          <div key={item.head} className="p-5 m-3 mt-12 rounded-2xl border-4">
+          <div key={item.head} className="p-10 m-3 mt-12 rounded-2xl border-4">
             <div>{item.icon}</div>
-            <p className="my-3 text-xl font-semibold">{item.head}</p>
-            <p className="m-auto max-w-90">{item.text}</p>
+            <p className="my-7 text-xl font-semibold tracking-widest">{item.head}</p>
+            <p className="m-auto">{item.text}</p>
           </div>
         );
       })}
--- src/components/layout/GuestLayout.tsx
+++ src/components/layout/GuestLayout.tsx
@@ -7,8 +7,10 @@ type Props = {
 
 export const GuestLayout = (props: Props) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
+    <div className="flex z-50 flex-col min-h-screen">
+      <div className="sticky top-0 z-50">
+        <Header />
+      </div>
       <div className="flex-1 min-h-screen">{props.children}</div>
       <Footer />
     </div>
