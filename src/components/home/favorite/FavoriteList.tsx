d6cfb9e273f741f8d7a1e837c7422465caad1089
fumiee <fumie.yori@gmail.com>
2021-12-30 19:32:14 +0900

    ♻️ reduce fetch

--- src/components/home/favorite/FavoriteList.tsx
+++ src/components/home/favorite/FavoriteList.tsx
@@ -16,24 +16,34 @@ export const FavoriteList: VFC = () => {
       <div>
         {favoritePosts?.map((post) => {
           return (
-            <div key={post.postId} className="mb-10 bg-gray-100">
+            <div key={post.id} className="mb-10 bg-gray-100">
               <div className="flex justify-between min-w-max bg-gray-200">
-                <Link href={`profile/${post.post.user.userId}`}>
+                <Link href={`profile/${post.favoritsPost.user.userId}`}>
                   <a className="flex my-1 mx-2">
-                    {post.post.user.avatar ? (
-                      <Image src={post.post.user.avatar} alt="avatar" height={45} width={45} className="rounded-full" />
+                    {post.favoritsPost.user.avatar ? (
+                      <Image
+                        src={post.favoritsPost.user.avatar}
+                        alt="avatar"
+                        height={45}
+                        width={45}
+                        className="rounded-full"
+                      />
                     ) : (
                       <div className="bg-gray-100 rounded-full sm:w-28 sm:h-28" />
                     )}
-                    <div className="flex items-center mx-3 text-sm">{post.post.user.name}</div>
+                    <div className="flex items-center mx-3 text-sm">{post.favoritsPost.user.name}</div>
                   </a>
                 </Link>
                 <div className="flex items-center space-x-4">
-                  {post.post.user.userId === userId ? <EditPageLinkButton id={post.id} /> : <div className="w-6"></div>}
+                  {post.favoritsPost.user.userId === userId ? (
+                    <EditPageLinkButton id={post.id} />
+                  ) : (
+                    <div className="w-6"></div>
+                  )}
                   <CommentButton postId={post.id} />
-                  <LikeButton postId={post.postId} likes={likes} setLikes={setLikes} />
+                  <LikeButton postId={post.favoritsId} likes={likes} setLikes={setLikes} />
                   <FavoriteButton
-                    postId={post.postId}
+                    postId={post.favoritsId}
                     favoritePostsArray={favoritePostsArray}
                     setFavoritePostsArray={setFavoritePostsArray}
                   />
@@ -41,11 +51,8 @@ export const FavoriteList: VFC = () => {
               </div>
               <details className="block whitespace-pre-wrap break-words">
                 <summary className="list-none">
-                  <div className="px-2 text-left">{post.post.text.substr(0, 75)}</div>
+                  <div className="px-2 text-left">{post.favoritsPost.text}</div>
                 </summary>
-                {post.post.text.length > 67 ? (
-                  <div className="px-2 pb-1 text-left">{post.post.text.substr(75, 100000)}</div>
-                ) : null}
               </details>
             </div>
           );
--- src/hooks/useFetchLikeFav.tsx
+++ src/hooks/useFetchLikeFav.tsx
@@ -1,9 +1,75 @@
-import { useFetchLikes } from "src/hooks/useFetchLikes";
-import { useFetchFavorits } from "src/hooks/useFetchFavorits";
+// import type { FavPost } from "src/hooks/useFetchFavorits";
+import type { definitions } from "src/types/supabaseTypes";
+import { useEffect, useState } from "react";
+import { supabase } from "src/libs/supabase";
+
+export type FavPost = {
+  id: definitions["posts"]["id"];
+  favoritsId: definitions["favorits"]["post_id"];
+  favoritsPost: {
+    text: definitions["posts"]["text"];
+    user: {
+      name: definitions["profiles"]["name"];
+      avatar: definitions["profiles"]["avatar"];
+      userId: definitions["profiles"]["user_id"];
+    };
+  };
+};
 
 export const useFetchLikeFav = () => {
-  const { likes, setLikes } = useFetchLikes();
-  const { userId, favoritePostsArray, setFavoritePostsArray, favoritePosts } = useFetchFavorits();
+  //ユーザーがお気に入りに入れている投稿情報
+  const [favoritePosts, setFavoritePosts] = useState<FavPost[]>([]);
+  //ユーザーがお気に入りに入れている投稿のpost_idのみを配列にしたもの
+  const [favoritePostsArray, setFavoritePostsArray] = useState<string[]>([]);
+  //ユーザーがいいねしている投稿情報
+  const [likes, setLikes] = useState<string[]>([]);
 
+  const userId = supabase.auth.user()?.id;
+  const fetch = async () => {
+    try {
+      const res = await supabase
+        .from("profiles")
+        .select(
+          `
+          likes:likes_user_id_fkey(
+            likesId:post_id
+          ),
+          favorits:favorits_user_id_fkey(
+            id,
+            favoritsId:post_id,
+            favoritsPost:favorits_post_id_fkey(
+              text,
+              user:posts_user_id_fkey(
+                name,
+                avatar,
+                userId:user_id
+              )
+            )
+          )  
+          `
+        )
+        .eq("user_id", userId)
+        .single();
+      if (res.error) throw res.error;
+
+      setLikes(
+        res.data.likes.map((like: any) => {
+          return like.likesId;
+        })
+      );
+      setFavoritePosts(res.data.favorits);
+      setFavoritePostsArray(
+        res.data.favorits.map((fav: any) => {
+          return fav.favoritsId;
+        })
+      );
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+  useEffect(() => {
+    fetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return { likes, setLikes, userId, favoritePostsArray, setFavoritePostsArray, favoritePosts };
 };
